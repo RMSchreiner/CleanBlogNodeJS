5cb018ec33bccb12a6218c4b49e39de2e7594fad
Randolph Schreiner <Rschreiner83@gmail.com>
2021-05-17 20:14:41 -0400

    imageAttempt

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const express = require("express");
 const path = require("path");
-const app = new express();
-const ejs = require("ejs");
-app.set("view engine", "ejs");
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-const BlogPost = require('./models/BlogPost.js')
-
 const mongoose = require("mongoose");
-//mongoose.connect(process.env.DATABASE_URL, {
+
 mongoose.connect("mongodb://localhost/my_database", {
   useNewUrlParser: true,
 });
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to Mongoose"));
+
+
+const app = new express();
+const ejs = require("ejs");
+
+const fileUpload = require('express-fileupload')
+
+const BlogPost = require('./models/BlogPost.js')
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(fileUpload())
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
 });
+
+const { start } = require("repl");
+//mongoose.connect(process.env.DATABASE_URL, {
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 
 app.get("/", async(req, res) => {
     const blogposts = await BlogPost.find({})
@@ -36,49 +45,22 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
-app.get("/post", (req, res) => {
-  res.render("post");
+app.get("/post/:id",async (req,res) => {
+  const blogpost = await BlogPost.findById(req.params.id)
+  res.render('post',{
+    blogpost
+  })
 });
+
 app.get("/posts/new", (req, res) => {
   res.render("create");
 });
-app.post('/posts/store', async(req,res) =>{
+
+app.post('/posts/store',async (req,res) =>{
+  let image = req.files.image;
+  image.mv/(path.resolve(__dirname,'public/img',image.name),async(error)=>{
     //model creates a new doc with browser data
-    await BlogPost.create(req.body)
-    res.redirect('/')
-})
-
-/*  search  
-app.get('/search',(req,res)=>{  
-  try {  
-  bookModel.find({$or:[{author:{'$regex':req.query.dsearch}},{books:{'$regex':req.query.dsearch}}]},(err,data)=>{  
-  if(err){  
-  console.log(err);  
-  }else{  
-  res.render('pages/home',{data:data});  
-  }  
-  })  
-  } catch (error) {  
-  console.log(error);  
-  }  
-  });  
-  app.post('/',(req,res)=>{  
-  try {  
-  var books = new bookModel({  
-  author:req.body.author,  
-  books:req.body.book  
-  });  
-  books.save((err,data)=>{  
-  if(err){  
-  console.log(err)  
-  }else{  
-  res.redirect('/');  
-  }  
-  })  
-  } catch (error) {  
-  console.log(error);  
-  }  
-  });
-  */
-
-
+    await BlogPost.create({...req.body,image:'/img/'+image.name})
+    res.redirect('/');
+});
+});
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -4,7 +4,13 @@ const Schema= mongoose.Schema;
 
 const BlogPostSchema = new Schema({
 title:String,
-body:String
+body:String,
+username:String,
+datePosted:{/*cant declare property type with an object like this because we need 'defualt'*/
+type:Date,
+default: new Date()
+},
+image:String
 });
 
 const BlogPost = 
